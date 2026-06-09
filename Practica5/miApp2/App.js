95769bae2c5ed1fdb21f59f0527b6407cd4a5e14
Jairo <123046186@upq.edu.mx>
2026-06-08 18:56:57 -0600

    ComponentesRN

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/* Zona1: Importaciones de archivos y componentes */
+
+import { StatusBar } from 'expo-status-bar';
+/* import { Component } from 'react';*/
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
+
+/* Zona 2: Main - componentes */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola Mundo React Native :D</Text>
+      <Text>-----------------------------------</Text>
+      <Saludo/>
+      <Text>-----------------------------------</Text>
+      <Saludo2/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+/* Zona 3: Estilos y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
