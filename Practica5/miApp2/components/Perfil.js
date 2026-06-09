bff79f5f6d86699754a4cd02f9603bd11f0e422f
Jairo <123046186@upq.edu.mx>
2026-06-08 22:56:00 -0600

    Practica6

---
+++ Practica5/miApp2/components/Perfil.js
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+import { View } from 'react-native-web'
+
+export const Perfil = () => {
+    return (
+        <View>
+            <Text>Nombre: Erick Gutierrez Gutierrez</Text>
+            <Text>Carrera: Ingeniería en Sistemas</Text>
+            <Text>Materia: Programación móvil</Text>
+            <Text>Cuatrimestre: 9</Text>
+        </View>
+    )
+}
